ab4cf8504f7e0bf3c94233c9f093d253e2a629ac
JCho24 <johnc415@deltalearns.ca>
2020-10-20 12:50:29 -0700

    .

---
+++ assignment.py
@@ -0,0 +1,20 @@
+import time as t
+import pyautogui as p
+
+
+def button():
+    # Locate the off button and move to it
+
+    offlist = p.locateCenterOnScreen("ButtonOff.png")
+    offmove = p.moveTo(x = offlist[0], y = offlist[1], duration = 0.2)
+    print("Moved to the button!")
+
+    # Now we start clicking on the On button
+    
+    #onlist = p.locateCenterOnScreen("ButtonOn.png")
+    onbuttonclick = p.click(x=offlist[0], y=offlist[1], clicks=10, interval=0.13, button='left')
+    print("CLICKING BUTTON!!!")
+
+    return onbuttonclick
+
+button()
